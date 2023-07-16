40a9d5d12a2a8b6e9738070a2de3354ff2092ee4
Arjun <arjundasback515@gmail.com>
2023-07-16 23:52:57 +0600

    server create and running on port

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+const port=5000;
+
+app.get('/',(req,res)=>{
+    res.send("request server running ")
+})
+
+app.listen(port,()=>{
+    console.log(`server runnig on ${port}`);
+})
